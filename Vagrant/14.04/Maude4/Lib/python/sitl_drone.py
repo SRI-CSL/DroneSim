dab5824e50d3d9a808fde581729a889aaa5b51a1
Carolyn Talcott <clt@csl.sri.com>
2016-08-13 16:36:32 -0700

    ..

--- Vagrant/14.04/Maude4/Lib/python/sitl_drone.py
+++ Vagrant/14.04/Maude4/Lib/python/sitl_drone.py
@@ -6,59 +6,27 @@ from pymavlink import mavutil
 import subprocess
 from pymavlink import fgFDM
 import util
+ 
 
-# (define b0 (apply mkdrone "b0" "0" "0" "0" "14" "15"))
-# (invoke b0 "mv" "-7.163147" "-34.817705" "2" "23")
-# (invoke b0 "mv" "7.163147" "-3.818550" "2" "23")
-# b0
-def get_location_metres(original_location, dNorth, dEast):
-    """
-    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
-    specified `original_location`. The returned LocationGlobal has the same `alt` value
-    as `original_location`.
-    The function is useful when you want to move the vehicle around specifying locations relative to 
-    the current vehicle position.
-    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
-    For more information see:
-    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-    """
-    earth_radius = 6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+from subprocess import *
 
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    if type(original_location) is LocationGlobal:
-        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
-    elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
-    else:
-        raise Exception("Invalid Location object passed")
-        
-    return targetlocation;
+from sandbox import *
+from tmpglob import *
 
+dkargs = [ 'dronekit-sitl',
+           'copter',
+           '--home=-7.162675,-34.817705,36,250' ]
 
-def get_distance_metres(aLocation1, aLocation2):
-    """
-    Returns the ground distance in metres between two LocationGlobal objects.
-    This method is an approximation, and will not be accurate over large distances and close to the 
-    earth's poles. It comes from the ArduPilot test code: 
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """
-    dlat = aLocation2.lat - aLocation1.lat
-    dlong = aLocation2.lon - aLocation1.lon
-    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
-
-class WindParam(object):
-
-    def __init__(self,name):
-
-        self.name = name
-        self.speed = 0.0
-        self.direction = 0.0
-        self.turbulance = 0.0
+mpargs = [ 'mavproxy.py',
+           '--master',
+           'tcp:127.0.0.1:5760',
+           '--sitl=127.0.0.1:5501',
+           '--out=127.0.0.1:14550',
+           '--out=127.0.0.1:14551',
+           '--map',
+           '--console',
+           '--aircraft',
+           'test' ]
 
 
 class SimpleDrone(object):
@@ -73,14 +41,54 @@ class SimpleDrone(object):
         self.v = 0
         self.e = 10.0
         self.vehicle = None
-        self.windParam = WindParam("newWind")
+        #self.windParam = WindParam("newWind")
         self.altitude = 5
+
+        self.dronekit = None
+        self.mavproxy = None
         
-    def initialize(self, windstr):
+    def initialize(self):
+
+        self.spawn()
 
         self.vehicle = connect('127.0.0.1:14550', wait_ready=True)
 
-        self.vehicle.wind = util.Wind(windstr)
+
+        return True
+
+    def spawn(self):
+        sys.stderr.write("Spawning dronekit\n")
+        self.dronekit = SandBox('dronekit', dk_argv, False)
+        self.dronekit.start()
+        sys.stderr.write("dronekit with pid {0} spawned\n".format(self.dronekit.getpid()))
+
+        sys.stderr.write("sleeping\n")
+        time.sleep(2)
+        sys.stderr.write("slept\n")
+
+        
+        sys.stderr.write("Spawning mavproxy\n")
+        self.mavproxy =  SandBox('mavproxy', mp_argv, True)
+        self.mavproxy.start()
+        sys.stderr.write("mavproxy with pid {0} spawned\n".format(self.mavproxy.getpid()))
+
+    def exit(self):
+
+        if self.mavproxy is not None:
+            self.mavproxy.stop()
+            sys.stderr.write("mavproxy with pid {0} killed\n".format(self.mavproxy.getpid()))
+            self.mavproxy = None
+
+        if self.dronekit is not None:
+            self.dronekit.stop()
+            sys.stderr.write("dronekit with pid {0} killed\n".format(self.dronekit.getpid()))
+            self.dronekit = None
+
+
+
+								
+    def takeOff(self,alt):
+			  self.altitude = alt
         while not self.vehicle.is_armable:
             time.sleep(1)
         self.vehicle.mode = VehicleMode("GUIDED")
@@ -90,15 +98,15 @@ class SimpleDrone(object):
 
         self.vehicle.simple_takeoff(self.altitude)
 
+"""
         while True:
             print " Altitude: ", self.vehicle.location.global_relative_frame.alt
             if self.vehicle.location.global_relative_frame.alt >= self.altitude*0.95:
                 print "Reached target altitude"
                 break
             time.sleep(1)
-
-        return True
-
+"""
+ 
     def mv(self, x, y, z, v):
         # while True:
         #     print " Altitude: ", self.vehicle.location.global_relative_frame.alt
@@ -180,6 +188,7 @@ class SimpleDrone(object):
         dz = dz / vel
         return '{0} {1} {2} {3} {4} {5} {6} {7}'.format(pos[0], pos[1], pos[2], dx, dy, dz, vel, bat)
 
+"""
 x = SimpleDrone("hello")
 x.initialize("0,0,0")
 # x.initialize("10,1.57,0")
@@ -221,5 +230,6 @@ print "Battery: ", x.vehicle.battery.level
 # (lat,lon) = get_location_metres(latH,lonH,y,x)
 
 
+"""
 
 
