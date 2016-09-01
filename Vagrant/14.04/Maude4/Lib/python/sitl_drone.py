89ef05c9d0925ff902714f32d7debcd4fde1775c
Vivek Nigam <vivek@viveks-mbp.speedport_w_921v_1_39_000>
2016-09-01 10:28:23 +0200

    Adding SCP machinery

--- Vagrant/14.04/Maude4/Lib/python/sitl_drone.py
+++ Vagrant/14.04/Maude4/Lib/python/sitl_drone.py
@@ -99,8 +99,8 @@ class SimpleDrone(object):
                    '--sitl=127.0.0.1:{0}'.format(5501 + self.pipeincr),
                    '--out=127.0.0.1:{0}'.format(14550 + self.pipeincr),
                    '--aircraft',
-                   '--console',
                    '--map',           
+                   '--console',
                    '/tmp/drone_{0}'.format(self.ino) ]
         if self.debug:
             mpargs.extend(['--map', '--console'])
